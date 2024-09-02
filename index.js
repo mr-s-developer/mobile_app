ed662d3f75101ecea7c8b48cca7f4060ae746b6e
Sergey <sergey.sokolov.ssa@gmail.com>
2024-09-02 22:32:29 +0500

    CHANGED: Remove unnecessary code

--- index.js
+++ index.js
@@ -17,12 +17,10 @@ const database = getDatabase(app)
 console.log(database)
 
 let myLeads = []
-let oldLeads = []
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
 const inputBtn = document.getElementById("input-btn")
 const deleteBtn = document.getElementById("delete-btn")
-const tabBtn = document.getElementById("tab-btn")
 const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 
 if (leadsFromLocalStorage) {
@@ -55,12 +53,4 @@ inputBtn.addEventListener("click", function() {
   inputEl.value = ""
   localStorage.setItem("myLeads", JSON.stringify(myLeads) )
   render(myLeads)
-})
-
-tabBtn.addEventListener("click", function(){    
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-    myLeads.push(tabs[0].url)
-    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-    render(myLeads)
-  })
 })
